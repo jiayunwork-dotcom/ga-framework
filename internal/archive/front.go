ac9e32dc13ee5713e034f429771c183e474d2b0b
jiayun <jiayun_work@163.com>
2026-08-20 17:16:52 +0800

    fix: JYY-0208

--- internal/archive/front.go
+++ internal/archive/front.go
@@ -2,6 +2,7 @@ package archive
 
 func unwindDominated(domCount []int, dominated [][]int, i int, next *[]int) {
 	for _, j := range dominated[i] {
+		domCount[j]--
 		if domCount[j] == 0 {
 			*next = append(*next, j)
 		}
